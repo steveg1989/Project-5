26adb5f6eeaf47ec12c638302c74c96a9975c4ab
steveg1989 <steveg1989@gmail.com>
2021-12-13 15:53:08 -0500

    js file added

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,1 +1,7 @@
-
+function main(){
+  const idNode = document.getElementById("orderId");
+  idNode.innerText = localStorage.getItem("orderId");
+  console.log(localStorage.getItem("orderId"))
+  localStorage.clear();
+}
+main();
